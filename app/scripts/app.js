790d9c158db0ad779584599bc5f4f2fcb15492a0
Amber Caldwell <acaldwell710@gmail.com>
2017-05-02 21:28:55 -0400

    added controllers & state

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+     function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+            enabled: true,
+            requireBase: false
+         });
+         
+              $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         });
+     }
+ 
+
+angular
+    .module('BlocBaseProject', ['ui.router', 'firebase']);
+    .config(config);
+ })();
