1db36550253e239efc032a6926d35d3f7b9b0181
Amber Caldwell <acaldwell710@gmail.com>
2017-05-17 14:49:08 -0400

    trying to display messages beneath room title

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,16 +1,28 @@
 
  (function() {
-     function LandingCtrl(Room) {
+     function LandingCtrl(Room, Message) {
          this.rooms = Room.all;
+         this.currentRoom = null;
+         this.currentMessages = null;
          this.createRoom = Room.add;
+         this.messages = Message.all;
+         this.amessages = Message.getByRoomId;
          
-         this.selectRoom = function (room) {
-             $location.path(room.link);
-             
+         
+         this.selectRoom = function(room) {
+             console.log(room);
+             this.currentRoom = room;
+             // update the current room to the room we clicked
+             // get the messages for that room
+             // display them on the page
+         }
+         this.selectMessage = function(message) {
+             console.log(message);
+             this.currentMessage = message;
          }
      }
  
      angular
          .module('blocBaseProject')
-         .controller('LandingCtrl', ['Room', LandingCtrl]);
+         .controller('LandingCtrl', ['Room', 'Message', LandingCtrl]);
  })();
--- app/scripts/services/Messages.js
+++ app/scripts/services/Messages.js
@@ -3,13 +3,20 @@
     var Message = {};
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
-
-    Message.getByRoomId = function(roomId) {
+      
+      Message.getByRoomId = function(roomId) {
+        ref.orderByChild("roomId").equalTo(roomId);};
+      
+      Message.all = messages;
+      
+      Message.add = function(roomId) {
+       messages.$add({ message: "roomID" });
+      }
+        // use orderByChild and equalTo to filter the message
+        // hint: use $firebaseArray to convert the data to an array
         // Filter the messages by their room ID.
-    };
 
     return Message;
-    };
   }
 
   angular
